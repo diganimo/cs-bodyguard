c996eb4080e13a5e96d3c00ee2b5215713a583e1
Mark Herrmann <mark.herrmann@mailbox.org>
2022-04-03 23:15:09 +0200

    implement aes-key-wrap

---
+++ lib/crypto/aesKeyWrap.ts
@@ -0,0 +1,160 @@
+// Implemented, following this specification: https://www.heise.de/netze/rfc/rfcs/rfc3394.shtml
+// testet, using this test vector: https://datatracker.ietf.org/doc/html/rfc3394#section-4.6
+// also tested with random values, checking the formular: key = unwrap(wrap(key, kek), kek)
+
+/* eslint-disable id-length */
+/* eslint-disable no-bitwise */
+
+import { Buffer } from 'buffer';
+import forge from 'node-forge';
+import { unauthenticException } from '../exceptions';
+
+const iv = Buffer.from('A6A6A6A6A6A6A6A6', 'hex');
+const paddingOf16Byte = Buffer.from('10101010101010101010101010101010', 'hex');
+
+const aesEncrypt = (key: Buffer, plaintext: Buffer): Buffer => {
+  const keyForgeBuffer = forge.util.createBuffer(key, 'raw');
+  const plainForgeBuffer = forge.util.createBuffer(plaintext, 'raw');
+  const cipher = forge.cipher.createCipher('AES-ECB', keyForgeBuffer);
+
+  cipher.start({});
+  cipher.update(plainForgeBuffer);
+  cipher.finish();
+
+  const encryptedHex = cipher.output.toHex();
+
+  // Drop last 16 byte since this is the padding (padding can't be disabled with node-forge, unfortunately)
+  return Buffer.from(encryptedHex, 'hex').slice(0, -16);
+};
+
+// Restore padding (padding can't be disabled with node-forge, unfortunately)
+const restorePadding = (ciphertextWithoutPadding: Buffer, key: Buffer): Buffer => {
+  const padding = aesEncrypt(key, paddingOf16Byte);
+
+  return Buffer.concat([ ciphertextWithoutPadding, padding ]);
+};
+
+const aesDecrypt = (key: Buffer, ciphertext: Buffer): Buffer => {
+  const ciphertextPadded = restorePadding(ciphertext, key);
+  const keyForgeBuffer = forge.util.createBuffer(key, 'raw');
+  const cipherForgeBuffer = forge.util.createBuffer(ciphertextPadded, 'raw');
+  const cipher = forge.cipher.createDecipher('AES-ECB', keyForgeBuffer);
+
+  cipher.start({});
+  cipher.update(cipherForgeBuffer);
+  cipher.finish();
+
+  const decryptedHex = cipher.output.toHex();
+
+  return Buffer.from(decryptedHex, 'hex');
+};
+
+const msb = (countOfBytes: number, buffer: Buffer): Buffer => buffer.slice(0, countOfBytes);
+
+const lsb = (countOfBytes: number, buffer: Buffer): Buffer => buffer.slice(buffer.length - countOfBytes, buffer.length);
+
+const calculateA = (B: Buffer, n: number, j: number, i: number): Buffer => {
+  const msbBuffer = msb(8, B);
+
+  const msbBigInt = msbBuffer.readBigUInt64BE(0);
+  const resultBigInt = msbBigInt ^ BigInt((n * j) + i);
+  const resultBuffer = Buffer.allocUnsafe(8);
+
+  resultBuffer.writeBigUInt64BE(resultBigInt, 0);
+
+  return resultBuffer;
+};
+
+const bForUnwrap = (kek: Buffer, A: Buffer, j: number, i: number, n: number, r: Buffer): Buffer => {
+  /* eslint-disable @typescript-eslint/naming-convention */
+
+  const tBigInt = BigInt((n * j) + i);
+  const aBigInt = A.readBigUInt64BE(0);
+  const aXorTBigInt = aBigInt ^ tBigInt;
+  const aXorTBuffer = Buffer.allocUnsafe(8);
+
+  aXorTBuffer.writeBigUInt64BE(aXorTBigInt, 0);
+
+  return aesDecrypt(kek, Buffer.concat([ aXorTBuffer, r ]));
+
+  /* eslint-enable @typescript-eslint/naming-convention */
+};
+
+const aesWrapKey = ({ key, kek }: { key: Buffer; kek: Buffer }): Buffer => {
+  const P: Buffer[] = [];
+  const R: Buffer[] = [];
+  const C: Buffer[] = [];
+  const n = key.length / 8;
+
+  for (let i = 0; i < n; i++) {
+    P[i] = key.slice(8 * i, 8 * (i + 1));
+  }
+
+  const aCopy = new Uint8Array(iv.length);
+
+  iv.copy(aCopy);
+  let A = Buffer.from(aCopy);
+
+  for (let i = 0; i < n; i++) {
+    R[i] = P[i];
+  }
+
+  for (let j = 0; j <= 5; j++) {
+    for (let i = 0; i < n; i++) {
+      const B = aesEncrypt(kek, Buffer.concat([ A, R[i] ]));
+
+      A = calculateA(B, n, j, i + 1);
+
+      R[i] = lsb(8, B);
+    }
+  }
+
+  C[0] = A;
+
+  for (let i = 1; i <= n; i++) {
+    C[i] = R[i - 1];
+  }
+
+  return Buffer.concat(C);
+};
+
+const aesUnwrapKey = ({ wrappedKey, kek }: { wrappedKey: Buffer; kek: Buffer }): Buffer => {
+  const n = (wrappedKey.length / 8) - 1;
+  const C: Buffer[] = [];
+  const R: Buffer[] = [];
+  const P: Buffer[] = [];
+
+  for (let i = 0; i <= n; i++) {
+    C.push(wrappedKey.slice(8 * i, 8 * (i + 1)));
+  }
+
+  let A = C[0];
+
+  for (let i = 1; i <= n; i++) {
+    R[i - 1] = C[i];
+  }
+
+  for (let j = 5; j >= 0; j--) {
+    for (let i = n - 1; i >= 0; i--) {
+      const B = bForUnwrap(kek, A, j, i + 1, n, R[i]);
+
+      A = msb(8, B);
+      R[i] = lsb(8, B);
+    }
+  }
+
+  if (!A.equals(iv)) {
+    throw unauthenticException();
+  }
+
+  for (let i = 0; i < n; i++) {
+    P[i] = R[i];
+  }
+
+  return Buffer.concat(P);
+};
+
+/* eslint-enable no-bitwise */
+/* eslint-enable id-length */
+
+export { aesWrapKey, aesUnwrapKey };
