7003b0b0b8e374a3ccee239a7eadedde8250758c
Mark Herrmann <mark.herrmann@mailbox.org>
2022-04-23 17:57:35 +0200

    improve random tests by mocking

--- test/crypto/core/randomTests.ts
+++ test/crypto/core/randomTests.ts
@@ -1,10 +1,11 @@
 import { assert } from 'assertthat';
+import forge from 'node-forge';
 import { getRandomBuffer } from '../../../lib/crypto/core/random';
 
 const lengthsToTest = [ 12, 32, 42 ];
 
 describe('Random', (): void => {
-  describe('normal cases', (): void => {
+  describe('length checks', (): void => {
     for (const length of lengthsToTest) {
       test(`returns ${length} bytes buffer.`, async (): Promise<void> => {
         const bytes = await getRandomBuffer({ length });
@@ -12,9 +13,7 @@ describe('Random', (): void => {
         assert.that(bytes.length).is.equalTo(length);
       });
     }
-  });
 
-  describe('edge cases', (): void => {
     test('returns one byte buffer on given length 0.', async (): Promise<void> => {
       const length = 0;
       const bytes = await getRandomBuffer({ length });
@@ -27,6 +26,21 @@ describe('Random', (): void => {
       const bytes = await getRandomBuffer({ length });
 
       assert.that(bytes.length).is.equalTo(1);
+    });
+  });
+
+  describe('mocked random test', (): void => {
+    test('returns expected buffer.', async (): Promise<void> => {
+      const testRandomString = '1234';
+
+      // eslint-disable-next-line no-undef
+      const randomMock = jest.spyOn(forge.random, 'getBytesSync');
+
+      randomMock.mockReturnValueOnce(testRandomString);
+
+      const bytes = await getRandomBuffer({ length: 4 });
+
+      assert.that(bytes).is.equalTo(Buffer.from(testRandomString, 'binary'));
     });
   });
 });
